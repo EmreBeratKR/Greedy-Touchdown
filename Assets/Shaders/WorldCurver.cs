5cd11d37f19555a5b85ffd4b2a611a386df77ee3
Emre Berat Kıran <doctoremre7@gmail.com>
2022-06-13 21:37:00 +0300

    Setting up the Old Materials

---
+++ Assets/Shaders/WorldCurver.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Shaders
+{
+    [CreateAssetMenu(menuName = "World Curver")]
+    public class WorldCurver : ScriptableObject
+    {
+        [SerializeField] private Material[] materialsToCurve;
+        [SerializeField, Range(-0.01f, 0.01f)] private float magnitude;
+        [SerializeField] private float peekDistance;
+        
+        
+        private static readonly int MagnitudeID = Shader.PropertyToID("_Magnitude");
+        private static readonly int PeekDistanceID = Shader.PropertyToID("_Peek_Distance");
+
+
+        private void OnValidate()
+        {
+            foreach (var material in materialsToCurve)
+            {
+                material.SetFloat(MagnitudeID, magnitude);
+                material.SetFloat(PeekDistanceID, peekDistance);
+            }
+        }
+    }
+}
