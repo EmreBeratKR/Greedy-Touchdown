8235d6456174e74a766641841a6f9aa1e49bff52
Emre Berat Kıran <doctoremre7@gmail.com>
2022-06-16 04:03:43 +0300

    App Settings

---
+++ Assets/Scripts/AppSettings.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class AppSettings : MonoBehaviour
+{
+    private void Start()
+    {
+        SetFPS();
+    }
+
+    private static void SetFPS()
+    {
+        if (!Application.isMobilePlatform) return;
+        
+        Application.targetFrameRate = 60;
+    }
+}
