2d227537569ac272b29a1b2a6798c75054bad3fa
Emre Berat Kıran <doctoremre7@gmail.com>
2022-06-17 16:26:43 +0300

    Particles
    - Epic Toon Fx pack Imported
    - Confetti for Level Complete

---
+++ Assets/Scripts/Particles/CompositeParticle.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Particles
+{
+    public class CompositeParticle : MonoBehaviour
+    {
+        [SerializeField] private ParticleSystem[] particleSystems;
+
+
+        public void Play()
+        {
+            foreach (var particleSystem in particleSystems)
+            {
+                particleSystem.Play();
+            }
+        }
+        
+        public void Stop()
+        {
+            foreach (var particleSystem in particleSystems)
+            {
+                particleSystem.Stop();
+            }
+        }
+    }
+}
