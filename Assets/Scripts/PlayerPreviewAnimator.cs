8bc8a2824b2eb1c48d79cfd420c2ed855050c2d8
Emre Berat Kıran <doctoremre7@gmail.com>
2022-06-18 17:55:28 +0300

    Fitting Room Preview Animation

---
+++ Assets/Scripts/PlayerPreviewAnimator.cs
@@ -0,0 +1,21 @@
+﻿using NaughtyAttributes;
+using UnityEngine;
+
+public class PlayerPreviewAnimator : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+
+    [SerializeField ,AnimatorParam(nameof(animator))] 
+    private int anger;
+    
+
+    public void OnCharacterTypeChanged()
+    {
+        PlayAnger();
+    }
+
+    private void PlayAnger()
+    {
+        animator.SetTrigger(anger);
+    }
+}
